d4ec1dfecc9307357e26e4df309bacce635d1990
Solovev Pavel <p.solovev@elewise.com>
2020-09-19 18:56:41 +0400

    add styles

--- src/components/book-list-item/book-list-item.js
+++ src/components/book-list-item/book-list-item.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./book-list-item.css";
 
 const BookListItem = ({ book }) => {
-  const { title, author } = book;
+  const { title, author, price, coverImage } = book;
   return (
-    <>
-      <span>{title}</span>
-      <span>{author}</span>
-    </>
+    <div className="book-list-item">
+      <div className="book-cover">
+        <img src={coverImage} alt="cover"/>
+      </div>
+      <div className="book-details">
+        <a href="#" className="book-title">{title}</a>
+        <span className="book-author">{author}</span>
+        <span className="book-price">{price}</span>
+        <button className="btn btn-info add-to-cart">Add to cart</button>
+      </div>
+    </div>
   );
 };
 
